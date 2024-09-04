cd73d5e330a5aced1cfb289bed39fe0a598e1f37
Alejandro <alejandro.solaleiva@gmail.com>
2024-09-04 19:49:55 -0300

    listar_infracciones e inspeccionar_infracciones

--- lib/infracciones.ex
+++ lib/infracciones.ex
@@ -1,8 +1,16 @@
 defmodule Libremarket.Infracciones do
 
   def detectar_infracciones() do
-    {:ok}
+    infraccion = :rand.uniform(100) <= 30
+
+    if infraccion do
+      {:error}
+    else
+      {:ok}
+    end
+
   end
+
 
 end
 
@@ -22,8 +30,16 @@ defmodule Libremarket.Infracciones.Server do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
 
-  def detectar_infracciones(pid \\ __MODULE__) do
-    GenServer.call(pid, :detectar_infracciones)
+  def detectar_infracciones(id \\ 0, pid \\ __MODULE__) do
+    GenServer.call(pid, {:detectar_infracciones, id})
+  end
+
+  def listar_infracciones(pid \\ __MODULE__) do
+    GenServer.call(pid, :listar_infracciones)
+  end
+
+  def inspeccionar(id \\ 0, pid \\ __MODULE__) do
+    GenServer.call(pid, {:inspeccionar, id})
   end
 
   # Callbacks
@@ -40,9 +56,19 @@ defmodule Libremarket.Infracciones.Server do
   Callback para un call :comprar
   """
   @impl true
-  def handle_call(:detectar_infracciones, _from, state) do
-    result = Libremarket.Infracciones.detectar_infracciones
-    {:reply, result, state}
+  def handle_call({:detectar_infracciones, id}, _from, state) do
+    result = Libremarket.Infracciones.detectar_infracciones()
+    {:reply, result, [{id, result} | state]}
+  end
+
+  @impl true
+  def handle_call(:listar_infracciones, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_call({:inspeccionar, id}, _from, state) do
+    raise "error"
   end
 
 end
