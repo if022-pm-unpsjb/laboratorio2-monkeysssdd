5647b16078a5531fcb759a2201820f9e706a35e1
Alejandro Solá <alejandro.solaleiva@gmail.com>
2024-10-03 23:45:25 -0300

    Cambio en donde se guardan los archivos que contienen el estado de los servidores

--- lib/persistencia.ex
+++ lib/persistencia.ex
@@ -1,11 +1,11 @@
 defmodule Libremarket.Persistencia do
   def escribir_estado(estado, nombre_servidor) do
-    File.write("estado_#{nombre_servidor}.txt", estado)
+    File.write("data/estado_#{nombre_servidor}.txt", estado)
   end
 
   #esto es porque sino lee el archivo todo como string y no como un map y se rompe
   def leer_estado(nombre_servidor) do
-    case File.read("estado_#{nombre_servidor}.txt") do
+    case File.read("data/estado_#{nombre_servidor}.txt") do
       {:ok, contenido} when contenido != "" ->
         {estado, _bindings} = Code.eval_string(contenido)
         {:ok, estado}
